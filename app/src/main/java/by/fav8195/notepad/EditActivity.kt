580070d877059bd1a444d0d5ab5b7211c0272eda
Work <filinkov@tut.by>
2022-05-23 17:13:32 +0300

    02-01 activiti

---
+++ app/src/main/java/by/fav8195/notepad/EditActivity.kt
@@ -0,0 +1,11 @@
+package by.fav8195.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_activity)
+    }
+}
--- app/src/main/java/by/fav8195/notepad/MainActivity.kt
+++ app/src/main/java/by/fav8195/notepad/MainActivity.kt
@@ -2,10 +2,32 @@ package by.fav8195.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import by.fav8195.notepad.databinding.ActivityMainBinding
+import by.fav8195.notepad.db.MyDbManager
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding//adding Binding
+    val myDbManager = MyDbManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)//adding Binding
+        setContentView(binding.root)//setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myDbManager.openDb()
+    }
+
+    fun onclickNew(view: View) {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
     }
 }
